1b449f8cf0e3cb371767011b7ae9a4f6481ec4e7
vslala <varunshrivastava007@gmail.com>
2015-08-11 23:47:49 +0530

    g-follow

--- post.php
+++ post.php
@@ -51,6 +51,7 @@ include 'layout/_top_nav.php';
                          data-share="true">
                     </div>
                     <g:plusone></g:plusone>
+                    <div class="g-follow" data-annotation="bubble" data-height="20" data-href="//plus.google.com/u/0/+Varunshrivastava007" data-rel="author"></div>
                 </div>
             </div>
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
